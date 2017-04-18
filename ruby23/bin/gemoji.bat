c5bf51d53cfbf14967a8c70b29e9ad04fdcc5962
Thymen Heersmink <thymen@stijlgenoten.nl>
2017-04-18 08:53:14 +0200

    Alle files

---
+++ ruby23/bin/gemoji.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+IF NOT "%~f0" == "~f0" GOTO :WinNT
+@"C:\tools\ruby23\bin\ruby.exe" "C:/tools/ruby23/bin/gemoji" %1 %2 %3 %4 %5 %6 %7 %8 %9
+GOTO :EOF
+:WinNT
+@"C:\tools\ruby23\bin\ruby.exe" "%~dpn0" %*
